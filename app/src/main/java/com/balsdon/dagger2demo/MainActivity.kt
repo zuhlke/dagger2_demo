e8719427cc1927e203cdd3479bd9415b92ca2994
Quintin Balsdon <qbalsdon@gmail.com>
2018-07-04 18:56:51 +0100

    Added common presenter

--- app/src/main/java/com/balsdon/dagger2demo/MainActivity.kt
+++ app/src/main/java/com/balsdon/dagger2demo/MainActivity.kt
@@ -5,15 +5,21 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainView {
-    private val presenter = MainPresenter(this)
+    private val presenter = MainPresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        presenter.attach(this)
         buttonToClick.setOnClickListener { presenter.clicked() }
     }
 
     override fun setText(text: String) {
         buttonToClick.text = (getString(R.string.click_counter)).replace("0", text)
+    }
+
+    override fun onDestroy() {
+        presenter.detach()
+        super.onDestroy()
     }
 }
